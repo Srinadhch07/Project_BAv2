32022f315a885bb75442fb5a0fa9d50ac96a3d29
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2024-03-29 19:43:02 +0530

    Update LoginInterface.py

--- LoginInterface.py
+++ LoginInterface.py
@@ -28,9 +28,9 @@ listen_music=["https://youtube.com/@7clouds",
 listen_news=["https://youtube.com/@etvtelangana",
 "https://youtube.com/@BBCNews",
 "https://youtube.com/@V6NewsTelugu"]
+#KEY=AIzaSyD9a47SJ5kKfmMKpX8A2c5ox_CsYp_E52o
 
-
-genai.configure(api_key="AIzaSyD9a47SJ5kKfmMKpX8A2c5ox_CsYp_E52o")
+genai.configure(api_key="YOUR_API_KEY")
 # Set up the model
 generation_config = {
   "temperature": 0.9,
